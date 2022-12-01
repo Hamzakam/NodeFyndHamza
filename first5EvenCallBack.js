6927c7ece67c61d421965601418952dc2c487c4e
Hamzakam <mail.hamzakam@gmail.com>
2022-12-01 19:30:29 +0530

    Added closureExample and first5Even with callback

---
+++ first5EvenCallBack.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+
+function readFirstFiveCallback(error, fileData) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const Lines = fileData.toString().split("\n");
+  Lines.slice(0, 5).forEach((line) => {
+    console.log(line);
+  });
+}
+
+fs.readFile("./static/files/tenEvenNumbers", readFirstFiveCallback);
