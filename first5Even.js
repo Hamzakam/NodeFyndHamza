f3cff81a51e87fb8f1d7d4ab1f64a14042cd9c62
Hamzakam <mail.hamzakam@gmail.com>
2022-12-01 19:08:27 +0530

    Added the first five print even numbers from a file with 10 numbers

--- first5Even.js
+++ first5Even.js
@@ -1,1 +1,12 @@
 //Create a callback function that will read the first 5 even numbers from a file containing 10 even numbers.
+const fs = require("fs").promises;
+
+async function readFirst5Even() {
+  const file = await fs.readFile("./static/files/tenEvenNumbers.txt");
+  const line = file.toString().split("\n");
+  for (let i = 0; i < 5; i++) {
+    console.log(line[i]);
+  }
+}
+
+readFirst5Even();
